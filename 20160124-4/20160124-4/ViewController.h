7dda37ced2526615320556eee96c302d68f970f7
陈欣 <95323362@qq.com>
2016-01-24 21:49:58 +0800

    20160124-4

---
+++ 20160124-4/20160124-4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160124-4
+//
+//  Created by ChenXin on 16/1/24.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160124-4/20160124-4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160124-4
+//
+//  Created by ChenXin on 16/1/24.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
