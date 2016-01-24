7e0250f516031c720a57c4ae86cd5d0cab6c2cf6
陈欣 <95323362@qq.com>
2016-01-24 19:59:30 +0800

    试试xcode里面的git commit

---
+++ 20160124-3/20160124-3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160124-3
+//
+//  Created by ChenXin on 16/1/24.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160124-3/20160124-3/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  20160124-3
+//
+//  Created by ChenXin on 16/1/24.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *haha;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)hehe:(id)sender {
+}
+
+@end
