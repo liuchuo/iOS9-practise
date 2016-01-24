21b009c28f089da3e9e293b0b7f49d4035bacea6
陈欣 <95323362@qq.com>
2016-01-24 14:44:11 +0800

    test

---
+++ 20160123-4.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+let str1 = "ling, yi, er, san, si, wu, liu"
+let b = str1.characters.count
+let d = str.characters.count
+print("str has \(d) characters")
+
