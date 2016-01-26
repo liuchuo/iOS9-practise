d75a49d85f34c139034f70c98e1d2765d142f7c1
陈欣 <95323362@qq.com>
2016-01-27 02:01:55 +0800

    啥也没写

---
+++ 十二生肖/十二生肖/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  十二生肖
+//
+//  Created by ChenXin on 16/1/26.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
