7281d575a645203311a4509f061c72b3dbad4fa3
陈欣 <95323362@qq.com>
2016-01-26 22:53:29 +0800

    就是看一下一个空的工程里面都有哪些新生成的文件

---
+++ 工程的名字/工程的名字/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  工程的名字
+//
+//  Created by ChenXin on 16/1/26.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 工程的名字/工程的名字/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  工程的名字
+//
+//  Created by ChenXin on 16/1/26.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
