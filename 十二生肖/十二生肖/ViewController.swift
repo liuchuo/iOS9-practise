1374631a6ec60a8d48e308d6c0790760ed662378
陈欣 <95323362@qq.com>
2016-01-27 19:47:12 +0800

    MOOC第一课十二生肖完成

--- 十二生肖/十二生肖/ViewController.swift
+++ 十二生肖/十二生肖/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        yearOfBirth.resignFirstResponder()
+    }
+    
+    @IBAction func OKtapped(sender: AnyObject) {
+        yearOfBirth.resignFirstResponder()
+        //当该代码执行的时候写yearOfBirth的TextField失去焦点-resign辞职
+        
+        if let year = Int(yearOfBirth.text!) {
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String(imageNumber))
+            //图片区域的名字.image = UIImage(named: 图片的名字//是个字符串)
+        } else {
+            //notify the user
+        }
+    }
 
 }
 
