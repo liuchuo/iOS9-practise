e689ee68ab5531dced16a1d55069ffe90f665079
陈欣 <95323362@qq.com>
2016-01-30 22:50:59 +0800

    button type test

---
+++ Button Type test/Button Type test/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Button Type test
+//
+//  Created by ChenXin on 16/1/30.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func hideKeyboard(sender: AnyObject) {
+    }
+
+}
+
