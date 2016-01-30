69a7f92f181797a6798e73be253017efbea8e11c
陈欣 <95323362@qq.com>
2016-01-30 14:32:51 +0800

    MOOC第二课 LoveFinder 完成

--- LoveFinder/LoveFinder/ViewController.swift
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -5,22 +5,65 @@
 //  Created by ChenXin on 16/1/28.
 //  Copyright © 2016年 ChenXin. All rights reserved.
 //
-
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var gender: UISegmentedControl!
+    
+    @IBOutlet weak var birthday: UIDatePicker!
+    
+    @IBOutlet weak var heightNumber: UISlider!
+    
+    @IBOutlet weak var height: UILabel!
+    
+    @IBOutlet weak var hasProperty: UISwitch!
+    
+    @IBOutlet weak var result: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        name.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func heightChanged(sender: AnyObject) {
+        // round the step to int
+        var slider = sender as! UISlider
+        var i = Int(slider.value)
+        slider.value = Float(i)
+        height.text = "\(i)cm"
+    }
+    
+    
+    @IBAction func okTapped(sender: AnyObject) {
+        let genderText = gender.selectedSegmentIndex == 0 ? "高富帅" : "白富美"
+        
+        let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
 
+        let now = NSDate()
+        
+        let components = gregorian?.components(NSCalendarUnit.Year, fromDate: birthday.date, toDate: now, options: NSCalendarOptions(rawValue: 0))
+        
+        let age = components?.year
+        
+        let hasPropertyText = hasProperty.on ? "有房" : "没房"
+        
+        result.text = "\(name.text), \(age!)岁, \(genderText), 身高\(height.text!), \(hasPropertyText), 求交往！"
+    }
 
+    //UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
