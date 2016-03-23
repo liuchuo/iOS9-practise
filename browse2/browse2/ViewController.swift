ce773d38ace23298270a0f554eb958267f57a420
陈欣 <95323362@qq.com>
2016-03-23 11:42:25 +0800

    -

---
+++ browse2/browse2/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  browse2
+//
+//  Created by ChenXin on 16/3/23.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var webview: UIWebView!
+    @IBOutlet var btnGo: UIButton!
+    @IBOutlet var urlInput: UITextField!
+    
+    @IBAction func btnGoClicked(sender : AnyObject) {
+        webview.loadRequest(NSURLRequest(URL: NSURL(string : urlInput.text!)!))
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
