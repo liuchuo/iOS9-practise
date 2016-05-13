20eea2f2c95e32de126da45aaec2ffc96b6cbd27
陈欣 <95323362@qq.com>
2016-05-14 06:29:29 +0800

    -

---
+++ custom/custom/prograsscontrol.swift
@@ -0,0 +1,21 @@
+//
+//  prograsscontrol.swift
+//  custom
+//
+//  Created by ChenXin on 16/5/10.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class prograsscontrol: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
